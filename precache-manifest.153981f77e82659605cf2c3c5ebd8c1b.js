1f85b30754d4d03c543c3207ce682e9e48041a8e
maeun <maeuniyee@naver.com>
2024-07-07 14:03:55 +0900

    Updates

---
+++ precache-manifest.153981f77e82659605cf2c3c5ebd8c1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1882d481d637c0254d82113bd02b69c8",
+    "url": "/poly_molly/index.html"
+  },
+  {
+    "revision": "fa0f186cb5f9a9600192",
+    "url": "/poly_molly/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "24f6fa62650d1328b93e",
+    "url": "/poly_molly/static/js/2.f7ebaa9b.chunk.js"
+  },
+  {
+    "revision": "fa0f186cb5f9a9600192",
+    "url": "/poly_molly/static/js/main.d04f410c.chunk.js"
+  },
+  {
+    "revision": "15e7368a5c45109cde52",
+    "url": "/poly_molly/static/js/runtime~main.709cfa7e.js"
+  }
+]);
